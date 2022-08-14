b11f0ae286e6f0c5d08031aaff0452a40f6e38ac
charin <charin.aewzipo@gmail.com>
2022-08-15 00:34:18 +0700

    make better

--- routes/product.js
+++ routes/product.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.post("/add", createproduct);
 router.put("/:id", verifyToken, updateproduct);
 router.delete("/:id", deleteProduct);
-router.get("/", getProductAll);
+// router.get("/", getProductAll);
 router.get("/random", randomProduct);
 router.get("/find/:id", getProduct);
 export default router;

c37d3d12ca1003e0831d746406b07925f0a83dda
charin <charin.aewzipo@gmail.com>
2022-08-19 01:56:13 +0700

    responsive

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Origin",
-    // "http://localhost:3000"
-    "https://client-e-2commerce.herokuapp.com"
+    "http://localhost:3000"
+    // "https://client-e-2commerce.herokuapp.com"
   );
   res.header(
     "Access-Control-Allow-Headers",
